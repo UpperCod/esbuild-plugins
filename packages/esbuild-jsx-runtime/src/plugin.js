992639e59da8c1bd489037c2fb8c5b691f128ce9
UpperCod <mtrujillo@outlook.cl>
2021-05-23 16:51:08 -0400

    add options.inject

--- packages/esbuild-jsx-runtime/src/plugin.js
+++ packages/esbuild-jsx-runtime/src/plugin.js
@@ -6,12 +6,14 @@ import MagicString from "magic-string";
  * @param {string} [options.jsxFactory]
  * @param {string} [options.jsxFragment]
  * @param {string} [options.jsxImportSource]
+ * @param {string} [options.inject]
  * @returns {import("esbuild").Plugin}
  */
 export default function pluginsJsxRuntime({
     jsxFactory = "_jsx",
     jsxFragment = "Fragment",
     jsxImportSource = "atomico",
+    inject = "",
 } = {}) {
     return {
         name: "esbuild-jsx-plugin",
@@ -22,7 +24,8 @@ export default function pluginsJsxRuntime({
                 const jsx = await readFile(args.path, "utf8");
                 const code = new MagicString(jsx);
                 code.prepend(
-                    `import { jsx as _jsx } from "${jsxImportSource}/jsx-runtime";`
+                    inject ||
+                        `import { jsx as _jsx } from "${jsxImportSource}/jsx-runtime";`
                 );
                 const map = code.generateMap();
                 return {
